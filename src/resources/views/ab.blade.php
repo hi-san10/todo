9ee670ca172ae59e18227701da9a921f3149094d
hi-san <h.ahkh10@icloud.com>
2024-04-27 12:09:47 +0900

    アップ

---
+++ src/resources/views/ab.blade.php
@@ -0,0 +1,10 @@
+            <div class="todo-form">
+                <input type="text">
+                <button class="todo-form__btn--blue">更新</button>
+                <button class="todo-form__btn--red">削除</button>
+            </div>
+            <div class="todo-form">
+                <input type="text">
+                <button class="todo-form__btn--blue">更新</button>
+                <button class="todo-form__btn--red">削除</button>
+            </div>
